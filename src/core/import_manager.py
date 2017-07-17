1bddf6af0c7cae0277c261669296f7c556fbb013
kernelland <kernelland@protonmail.ch>
2017-07-17 16:30:06 +0200

    Update import_manager.py

--- src/core/import_manager.py
+++ src/core/import_manager.py
@@ -1,10 +1,14 @@
 import re
 import src.core.database_manager  as database_manager
 import src.core.translate_manager as translate_manager
-
-
+import src.core.database_manager  as database_manager
         
-def filter_argument(argument):
+def filter_argument(argument,permission):
+    if argument in database_manager.sql_whats_in_store('data/db.db') and permission:
+        l = []
+        for i in database_manager.sql_read_data('data/db.db',argument):
+            l.append(i[-1])
+        return l
     argument = "".join(argument)
     contains_index = re.findall(r"\[([0-9_]+)\]",argument)
     if len(contains_index) > 0:
@@ -18,10 +22,10 @@ def filter_argument(argument):
     print(argument)
     
 
-def filter_two(argument):
+def filter_two(argument,permission=True):
     if len(argument) > 1:
         for index,_argument in enumerate(argument):
-             argument[index] = filter_argument(_argument)
+             argument[index] = filter_argument(_argument,permission)
         return argument
     else:
         return argument
@@ -45,23 +49,27 @@ def call_module(command,*args):
     
     try: 
         module = craft_module(command.split()[0])
+        #print(command.split()[0])
+        no_permission = ['_list']
+        if command.split()[0] in no_permission:
+            permission = False
+        else:
+            permission = True
         arg = translate_manager.filter_argument(command.split()[1])
         argument =  command.split()[1::]   
         filter_two(argument)
         if len(argument) == 1:
-            argument = filter_argument(argument)
-            
+            argument = filter_argument(argument,permission)
+        #print(permission)
         module(*argument)
     except IndexError:
         module()
     except TypeError:
-        argument = filter_argument(argument)
+        no_permission = ['_list']
+        if command.split()[0] in no_permission:
+            permission = False
+        else:
+            permission = True
+        argument = filter_argument(argument,permission)
         module(argument)
-
-
-        module()
-
-
-    except:
-        module()
 
